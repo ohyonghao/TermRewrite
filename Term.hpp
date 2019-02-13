d45606e64d71c6e8795d64a08402e23833ba3418
David Post <david@ohyonghao.com>
2019-02-13 15:46:49 -0800

    Trying it with raw pointers
    Changing to raw
    
    Signed-off-by: David Post <david@ohyonghao.com>

--- Term.hpp
+++ Term.hpp
@@ -2,7 +2,7 @@
 #define TERM_HPP
 
 #include <string>
-#include <list>
+#include <vector>
 #include <memory>
 #include <stack>
 
@@ -36,7 +36,7 @@ public:
     virtual bool operator!=(const term<T>& rhs)=0;
     virtual bool operator==(const term<T>& rhs) = 0;
 
-    virtual std::list< std::shared_ptr< term< T > > > children( )=0;
+    virtual std::vector< std::shared_ptr< term< T > > > children( )=0;
 
     virtual std::ostream& pp(std::ostream&) const=0;
 
@@ -74,8 +74,8 @@ public:
 
     std::string var(){ return _var; }
 
-    // No children, so return empty list
-    std::list< std::shared_ptr< term< T > > > children( ){return std::list< std::shared_ptr< term< T > > >();}
+    // No children, so return empty vector
+    std::vector< std::shared_ptr< term< T > > > children( ){return std::vector< std::shared_ptr< term< T > > >();}
     std::ostream& pp(std::ostream&) const;
 
     typedef T           value_type;
@@ -112,8 +112,8 @@ public:
     literal( T __value ):_value{__value}{}
     T& value(){return _value;}
     const T& value()const{return _value;}
-    // No children, so return empty list
-    std::list< std::shared_ptr< term< T > > > children( ){return std::list< std::shared_ptr< term< T > > >();}
+    // No children, so return empty vector
+    std::vector< std::shared_ptr< term< T > > > children( ){return std::vector< std::shared_ptr< term< T > > >();}
     std::ostream& pp(std::ostream&) const;
 
     typedef T           value_type;
@@ -143,21 +143,43 @@ private:
 /*!
  * \brief Class function, derived class that holds functions
  */
-/*
+
+
 template<typename T>
 class function: public term<T>{
 public:
-    function(std::string __name, uint32_t __arity, std::list<std::shared_ptr< term<T>>> __subterms );
+    function(std::string __name, uint32_t __arity, std::vector<std::shared_ptr< term<T>>> __subterms );
     std::string& name(){return _name;}
     const std::string& name()const{ return _name; }
-    std::list< std::shared_ptr< term< T > > > children( ){return _subterms;}
+    // No children, so return empty vector
+    std::vector< std::shared_ptr< term< T > > > children( ){return _subterms;}
     std::ostream& pp(std::ostream&) const;
+
+    typedef T           value_type;
+    typedef T*          pointer;
+    typedef T&          reference;
+    typedef size_t      size_type;
+    typedef ptrdiff_t   difference_type;
+    typedef term_iterator<T>        iterator;
+    typedef term_iterator<const T>  const_iterator;
+    typedef std::bidirectional_iterator_tag iterator_category;
+    typedef std::reverse_iterator<iterator> reverse;
+    typedef std::reverse_iterator<const_iterator> const_r;
+
+    iterator begin();
+    iterator end();
+    iterator cbegin() const;
+    iterator cend() const;
+    bool operator!=(const term<T>& rhs){return !(*this == rhs);}
+    bool operator==(const term<T>& /*rhs*/){return false;}
+    bool operator!=(const function<T>& rhs){return !(*this == rhs);}
+    bool operator==(const function<T>& rhs){return _name == rhs._name && _arity == rhs._arity && _subterms == rhs._subterms;}
 private:
     std::string _name;
     uint32_t _arity;
-    std::list< std::shared_ptr< term<T> > > _subterms;
+    std::vector< std::shared_ptr< term<T> > > _subterms;
 };
-*/
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Implementation: Term
@@ -234,41 +256,90 @@ term_iterator<T> literal<T>::cend() const{
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Implementation: Function
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
+
 template<typename T>
-function<T>::function(std::string __name, uint32_t __arity, std::list<std::shared_ptr< term<T>>> __subterms ):
+function<T>::function(std::string __name, uint32_t __arity, std::vector<std::shared_ptr< term<T>>> __subterms ):
     _name{__name},
     _arity{__arity},
     _subterms{__subterms}
 {}
-*/
+
+template<typename T>
+std::ostream& function<T>::pp(std::ostream& out) const{
+    out << _name ;
+    return out;
+}
+template<typename T>
+term_iterator<T> function<T>::begin(){
+    // Check for children, then move to the beginning
+    return term_iterator<T>(std::make_shared<function<T>>(*this));
+}
+
+template<typename T>
+term_iterator<T> function<T>::cbegin() const {
+    // Check for children, then move to the beginning
+    return term_iterator<T>(std::make_shared<function<T>>(*this));
+}
+
+
+template<typename T>
+term_iterator<T> function<T>::end(){
+    // Check for children, then move to the beginning
+    return term_iterator<T>(std::make_shared<function<T>>(*this));
+}
+
+template<typename T>
+term_iterator<T> function<T>::cend() const{
+    // Check for children, then move to the beginning
+    return term_iterator<T>(std::make_shared<function<T>>(*this));
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Implementation: term_iterator
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template<typename T>
+term_iterator<T>::term_iterator( std::shared_ptr<term<T> > __root ):
+    _root{__root},
+    current_iterator{this}
+{
+}
 
 // I'm thinking that for this I want to use a stack with pairs that are the term and
 // the child that I am on, most might be 1. Alternatively we might store iterators on the stack
 // instead.
 template<typename T>
 term_iterator<T>& term_iterator<T>::operator++(){
-    if(_root->children().empty())
-        done = true;
+    // If we've set the done flag, don't do anything
+    if( done ){
+        return *this;
+    }
+    if(isSelf()){
+        if( _root->children().empty() ){
+            done = true;
+            return *this;
+        }
+    }
+
+    // If not self, increment the iterator
+    while( ++*current_iterator != *current_iterator ){
+        // != ignores the rhs, but we do need something there, I know, it's silly
+        // After incrementing we want to check if we hit the end. If we did, then
+        // pop and increment the last iterator
+        current_iterator = _its.top();
+        _its.pop();
+    }
     return *this;
-}
-
-template<typename T>
-term_iterator<T>::term_iterator( std::shared_ptr<term<T> > __root ):
-    _root{__root}
-{
-
 }
 
 template<typename T>
 term<T>& term_iterator<T>::operator*() const
 {
-    return *_root;
+    if( _its.empty() ){
+        return *_root;
+    }else{
+        return **current_iterator;
+    }
 }
-
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 /// operator<< Streaming
