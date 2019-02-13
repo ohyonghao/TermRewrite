63a8b3fba493038fdeab2d2d25b23ee5aa58c7f2
David Post <david@ohyonghao.com>
2019-02-13 11:10:45 -0800

    First two terms are working with iterator
    Literal and Variable are now both working. They are essentially the same
    things, so copy and paste.
    
    Signed-off-by: David Post <david@ohyonghao.com>

---
+++ Term.hpp
@@ -0,0 +1,282 @@
+#ifndef TERM_HPP
+#define TERM_HPP
+
+#include <string>
+#include <list>
+#include <memory>
+#include <stack>
+
+template<typename T>
+class term_iterator;
+
+/*!
+ * \brief Class Term, base class for terms
+ */
+template<typename T>
+class term
+{
+
+public:
+    typedef T           value_type;
+    typedef T*          pointer;
+    typedef T&          reference;
+    typedef size_t      size_type;
+    typedef ptrdiff_t   difference_type;
+    typedef term_iterator<T>        iterator;
+    typedef term_iterator<const T>  const_iterator;
+    typedef std::bidirectional_iterator_tag iterator_category;
+    typedef std::reverse_iterator<iterator> reverse;
+    typedef std::reverse_iterator<const_iterator> const_r;
+
+    virtual iterator begin()=0;
+    virtual iterator end()=0;
+    virtual iterator cbegin() const=0;
+    virtual iterator cend() const=0;
+
+    virtual bool operator!=(const term<T>& rhs)=0;
+    virtual bool operator==(const term<T>& rhs) = 0;
+
+    virtual std::list< std::shared_ptr< term< T > > > children( )=0;
+
+    virtual std::ostream& pp(std::ostream&) const=0;
+
+};
+
+template<typename T>
+class term_iterator{
+private:
+    std::shared_ptr< term< T > > _root;
+
+    bool done{false};
+    //std::stack< term<T> > _path;
+public:
+    term_iterator(std::shared_ptr<term<T> >);
+    term<T>& operator*() const;
+    term<T>* operator->() const;
+    term_iterator& operator++();
+    term_iterator& operator--();
+    term_iterator operator++(int);
+    term_iterator operator--(int);
+    term_iterator& operator+=(unsigned int);
+    term_iterator& operator-=(unsigned int);
+    bool operator!=(const term_iterator &/*rhs*/){return !done;}
+    bool operator==(const term_iterator &/*rhs*/){return done;}
+};
+
+/*! ***************************************************************
+ * \brief Class variable, derived class that holds variables
+ * ***************************************************************
+ */
+template<typename T>
+class variable: public term<T>{
+public:
+    variable(std::string __var):_var{__var}{}
+
+    std::string var(){ return _var; }
+
+    // No children, so return empty list
+    std::list< std::shared_ptr< term< T > > > children( ){return std::list< std::shared_ptr< term< T > > >();}
+    std::ostream& pp(std::ostream&) const;
+
+    typedef T           value_type;
+    typedef T*          pointer;
+    typedef T&          reference;
+    typedef size_t      size_type;
+    typedef ptrdiff_t   difference_type;
+    typedef term_iterator<T>        iterator;
+    typedef term_iterator<const T>  const_iterator;
+    typedef std::bidirectional_iterator_tag iterator_category;
+    typedef std::reverse_iterator<iterator> reverse;
+    typedef std::reverse_iterator<const_iterator> const_r;
+
+    iterator begin();
+    iterator end();
+    iterator cbegin() const;
+    iterator cend() const;
+    bool operator!=(const term<T>& rhs){return !(*this == rhs);}
+    bool operator==(const term<T>& /*rhs*/){return false;}
+    bool operator!=(const variable<T>& rhs){return !(*this == rhs);}
+    bool operator==(const variable<T>& rhs){return _var == rhs._var;}
+
+private:
+    std::string _var;
+};
+
+/*!
+ * \brief Class literal, derived class that holds literals
+ */
+
+template<typename T>
+class literal: public term<T>{
+public:
+    literal( T __value ):_value{__value}{}
+    T& value(){return _value;}
+    const T& value()const{return _value;}
+    // No children, so return empty list
+    std::list< std::shared_ptr< term< T > > > children( ){return std::list< std::shared_ptr< term< T > > >();}
+    std::ostream& pp(std::ostream&) const;
+
+    typedef T           value_type;
+    typedef T*          pointer;
+    typedef T&          reference;
+    typedef size_t      size_type;
+    typedef ptrdiff_t   difference_type;
+    typedef term_iterator<T>        iterator;
+    typedef term_iterator<const T>  const_iterator;
+    typedef std::bidirectional_iterator_tag iterator_category;
+    typedef std::reverse_iterator<iterator> reverse;
+    typedef std::reverse_iterator<const_iterator> const_r;
+
+    iterator begin();
+    iterator end();
+    iterator cbegin() const;
+    iterator cend() const;
+    bool operator!=(const term<T>& rhs){return !(*this == rhs);}
+    bool operator==(const term<T>& /*rhs*/){return false;}
+    bool operator!=(const variable<T>& rhs){return !(*this == rhs);}
+    bool operator==(const variable<T>& rhs){return _value == rhs._value;}
+private:
+
+    T _value;
+};
+
+/*!
+ * \brief Class function, derived class that holds functions
+ */
+/*
+template<typename T>
+class function: public term<T>{
+public:
+    function(std::string __name, uint32_t __arity, std::list<std::shared_ptr< term<T>>> __subterms );
+    std::string& name(){return _name;}
+    const std::string& name()const{ return _name; }
+    std::list< std::shared_ptr< term< T > > > children( ){return _subterms;}
+    std::ostream& pp(std::ostream&) const;
+private:
+    std::string _name;
+    uint32_t _arity;
+    std::list< std::shared_ptr< term<T> > > _subterms;
+};
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Implementation: Term
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Implementation: Variable
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template<typename T>
+std::ostream& variable<T>::pp(std::ostream& out) const{
+    out << _var;
+    return out;
+}
+template<typename T>
+term_iterator<T> variable<T>::begin(){
+    // Check for children, then move to the beginning
+    return term_iterator<T>(std::make_shared<variable<T>>(*this));
+}
+
+template<typename T>
+term_iterator<T> variable<T>::cbegin() const {
+    // Check for children, then move to the beginning
+    return term_iterator<T>(std::make_shared<variable<T>>(*this));
+}
+
+
+template<typename T>
+term_iterator<T> variable<T>::end(){
+    // Check for children, then move to the beginning
+    return term_iterator<T>(std::make_shared<variable<T>>(*this));
+}
+
+template<typename T>
+term_iterator<T> variable<T>::cend() const{
+    // Check for children, then move to the beginning
+    return term_iterator<T>(std::make_shared<variable<T>>(*this));
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Implementation: Literal
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template<typename T>
+std::ostream& literal<T>::pp(std::ostream& out) const{
+    out << _value;
+    return out;
+}
+template<typename T>
+term_iterator<T> literal<T>::begin(){
+    // Check for children, then move to the beginning
+    return term_iterator<T>(std::make_shared<literal<T>>(*this));
+}
+
+template<typename T>
+term_iterator<T> literal<T>::cbegin() const {
+    // Check for children, then move to the beginning
+    return term_iterator<T>(std::make_shared<literal<T>>(*this));
+}
+
+
+template<typename T>
+term_iterator<T> literal<T>::end(){
+    // Check for children, then move to the beginning
+    return term_iterator<T>(std::make_shared<literal<T>>(*this));
+}
+
+template<typename T>
+term_iterator<T> literal<T>::cend() const{
+    // Check for children, then move to the beginning
+    return term_iterator<T>(std::make_shared<literal<T>>(*this));
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Implementation: Function
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+template<typename T>
+function<T>::function(std::string __name, uint32_t __arity, std::list<std::shared_ptr< term<T>>> __subterms ):
+    _name{__name},
+    _arity{__arity},
+    _subterms{__subterms}
+{}
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Implementation: term_iterator
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// I'm thinking that for this I want to use a stack with pairs that are the term and
+// the child that I am on, most might be 1. Alternatively we might store iterators on the stack
+// instead.
+template<typename T>
+term_iterator<T>& term_iterator<T>::operator++(){
+    if(_root->children().empty())
+        done = true;
+    return *this;
+}
+
+template<typename T>
+term_iterator<T>::term_iterator( std::shared_ptr<term<T> > __root ):
+    _root{__root}
+{
+
+}
+
+template<typename T>
+term<T>& term_iterator<T>::operator*() const
+{
+    return *_root;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// operator<< Streaming
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template<typename T>
+std::ostream& operator<<(std::ostream& out, const term<T>& rhs ){
+    rhs.pp( out );
+    return out;
+}
+#endif // TERM_HPP
---
+++ TermsTest.cpp
@@ -0,0 +1,135 @@
+#include "Term.hpp"
+#include "sub.hpp"
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+#include <memory>
+using namespace std;
+
+// In order to complete this assignment you'll need
+// a term class
+// template<typename T>
+// class term {...}
+//
+// three subclasses
+// template<typename T>
+// class literal : term<T>
+
+// template<typename T>
+// class variable : term<T>
+
+// template<typename T>
+// class function : term<T>
+
+
+// I'm making everything with shared pointers, because
+// we have to use pointers or references to do inheritance
+//
+//template<typename T>
+//using term_ptr = shared_ptr<term<T>>;
+template<typename T>
+using variable_ptr = shared_ptr<variable<T>>;
+template<typename T>
+using literal_ptr = shared_ptr<literal<T>>;
+/*template<typename T>
+using function_ptr = shared_ptr<function<T>>;
+*/
+// a rule is a pair of terms
+//template<typename T>
+//using rule = pair<term_ptr<T>, term_ptr<T>>;
+
+/////////////////////////////////
+// Boolean algebra
+/////////////////////////////////
+
+//variables in terms
+variable_ptr<bool> b_v() {return make_shared<variable<bool>>(variable<bool>("v"));}
+//variable_ptr<bool> b_w() {return make_shared<variable<bool>>(variable<bool>("w"));}
+//variable_ptr<bool> b_x() {return make_shared<variable<bool>>(variable<bool>("x"));}
+//variable_ptr<bool> b_y() {return make_shared<variable<bool>>(variable<bool>("y"));}
+//variable_ptr<bool> b_z() {return make_shared<variable<bool>>(variable<bool>("z"));}
+
+//literal values
+literal_ptr<bool> b_true() {return make_shared<literal<bool>>(literal<bool>(true));}
+//literal_ptr<bool> b_false() {return make_shared<literal<bool>>(literal<bool>(false));}
+
+//functions
+/*
+function_ptr<bool> b_and(term_ptr<bool> x, term_ptr<bool> y)
+{
+    return make_shared<function<bool>>(function<bool>("&&", 2, {x, y}));
+}
+function_ptr<bool> b_or(term_ptr<bool> x, term_ptr<bool> y)
+{
+    return make_shared<function<bool>>(function<bool>("||", 2, {x, y}));
+}
+function_ptr<bool> b_not(term_ptr<bool> x)
+{
+    return make_shared<function<bool>>(function<bool>("!", 1, {x}));
+}
+function_ptr<bool> b_arrow(term_ptr<bool> x, term_ptr<bool> y)
+{
+    return make_shared<function<bool>>(function<bool>("->", 2, {x,y}));
+}
+*/
+
+//variables for rules (to make sure there's no overlap)
+//variables for rewrite rules are a and b
+//variable_ptr<bool> b_a() {return make_shared<variable<bool>>(variable<bool>("a"));}
+//variable_ptr<bool> b_b() {return make_shared<variable<bool>>(variable<bool>("b"));}
+
+// the rule ->(a, false) => !(a)
+//rule<bool> contra{ make_pair(b_arrow(b_a(), b_false()), b_not(b_a())) };
+
+// the actual terms we'll be using
+//term_ptr<bool> b1 = b_or(b_and(b_x(), b_false()), b_y());
+//term_ptr<bool> b2 = b_or(b_and(b_true(), b_x()), b_arrow(b_or(b_v(), b_w()), b_false()));
+
+
+
+/////////////////////////////////
+// substitution
+/////////////////////////////////
+
+
+int main()
+{
+    cout << "print a var b_v" << endl;
+    for(auto &t : *b_v()){
+        cout << t << endl;
+    }
+    for(auto &t : *b_true()){
+        cout << t << endl;
+    }
+    /*
+    cout << "print term b1" << endl;
+    for(auto& t : *b1)
+    {
+        cout << t << endl;
+    }*/
+//    cout << "print term b2 backwards" << endl;
+    //for(auto t = b2->rbegin(); t != b2->rend(); t++)
+//    {
+        //cout << *t << endl;
+//    }
+
+    // make the substitution [a :-> or(v,w)]
+//    Sub<bool> sigma;
+//    sigma.extend("a", b_or(b_v(),b_w()));
+
+//    cout << "rewrite " << b1 << " with " << contra.second << endl;
+
+    // now use the rule ->(a, false) => !(a) to rewrite
+    // or(and(true,x), arrow(or(v,w), false)) to
+    // or(and(true,x), not(or(v,w)))
+//    term_ptr<bool> b1r = b1->rewrite(*contra.second, {2}, sigma);
+
+//    cout << b1r << endl;
+
+    // can I unify b1 with b2?
+//    Sub<bool> s;
+//    cout << "unifying b1 with b2? " << unify(*b1,*b2,s) << endl;
+//    s.print();
+
+    return 0;
+}
